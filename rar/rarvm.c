selmf/unarr

commit 55eb633b7b4c452e125d480d4efa69fa60195153

Rarvm: Don't pass null ptrs to memcpy
Resizing a rarvm program with a null opcode can lead to a situation where
a nullptr is passed to memcpy. As this is undefined behavior, check for
nullptr and return false if such a situation is reached.

rar/rarvm.c
@@ -49,6 +49,8 @@ bool RARProgramAddInstr(RARProgram *prog, uint8_t instruction, bool bytemode)
     if (prog->length + 1 >= prog->capacity) {
         /* in my small file sample, 16 is the value needed most often */
         uint32_t newCapacity = prog->capacity ? prog->capacity * 4 : 32;
+        if (!prog->opcodes)
+            return false;
         RAROpcode *newCodes = calloc(newCapacity, sizeof(*prog->opcodes));
         if (!newCodes)
             return false;

commit 8fa00c390179401f70280fd9e0b9dfc8380e7a5f

Rarvm: Fix regression introduced in previous commit
Commit 55eb633b (Rarvm: Don't pass null ptrs to memcpy, 2022-06-20) introduced
a mechanism to reject rarvm instructions with no opcodes. This lead to a
regression in extracting rar files, because some rarvm instructions were rejected
as invalid.

Solution: Don't mark nullptr opcodes as false, but also don't try to use them as
memcpy source.

rar/rarvm.c
@@ -49,13 +49,14 @@ bool RARProgramAddInstr(RARProgram *prog, uint8_t instruction, bool bytemode)
     if (prog->length + 1 >= prog->capacity) {
         /* in my small file sample, 16 is the value needed most often */
         uint32_t newCapacity = prog->capacity ? prog->capacity * 4 : 32;
-        if (!prog->opcodes)
-            return false;
         RAROpcode *newCodes = calloc(newCapacity, sizeof(*prog->opcodes));
-        if (!newCodes)
+        if (!newCodes) {
             return false;
-        memcpy(newCodes, prog->opcodes, prog->capacity * sizeof(*prog->opcodes));
-        free(prog->opcodes);
+        }
+        if (prog->opcodes) {
+            memcpy(newCodes, prog->opcodes, prog->capacity * sizeof(*prog->opcodes));
+            free(prog->opcodes);
+        }
         prog->opcodes = newCodes;
         prog->capacity = newCapacity;
     }